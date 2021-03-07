90b3583ec5611f267544b0dd0b3b086db3765a8b
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2021-03-07 17:16:33 +0200

    PhoneBookTask

--- 03. Authentication -Homework/02.Phonebook/app.js
+++ 03. Authentication -Homework/02.Phonebook/app.js
@@ -1,5 +1,74 @@
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById("btnLoad").addEventListener('click', getData)
+    document.getElementById("phonebook").addEventListener('click',deleteElement)
+    document.getElementById('btnCreate').addEventListener('click',createElement)
 }
 
 attachEvents();
+
+async function getData(){
+
+    const ul = document.getElementById("phonebook")
+    ul.innerHTML = ''
+    const response = await fetch(`http://localhost:3030/jsonstore/phonebook`);
+    const data = await response.json();
+
+    Object.values(data).map(x => {
+        const li = document.createElement('li')
+        const btnDel = document.createElement('button')
+        
+        li.textContent = `${x.person}:${x.phone}`
+        btnDel.textContent = 'Delete'
+        btnDel["id"] = x._id
+        
+        li.appendChild(btnDel)        
+        ul.appendChild(li)
+    })
+
+}
+
+async function createElement(e){
+    e.preventDefault();
+    const name = document.getElementById('person').value
+    const phone = document.getElementById('phone').value
+
+    if (name == '' || phone == ''){
+        alert("You should enter name and phone number")
+        return
+    }
+    const record = {
+        person: name,
+        phone: phone
+    }
+
+    await fetch(`http://localhost:3030/jsonstore/phonebook`,{
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(record)
+    });
+
+    document.getElementById('person').value = '';
+    document.getElementById('phone').value = '';
+    getData();
+    
+}
+
+async function deleteElement(e){
+
+    if(e.target.tagName != 'BUTTON'){
+        return
+    }
+    const key = e.target.parentNode.firstElementChild.id
+
+    const confirmed = confirm('Are you sure you want to delete this record?')
+
+    if(confirmed){
+        const response = await fetch('http://localhost:3030/jsonstore/phonebook/' + key,{  
+            method: 'delete'
+
+        });
+    }
+         
+    getData()
+
+}
--- 03. Authentication -Homework/03.Students/app.js
+++ 03. Authentication -Homework/03.Students/app.js
@@ -1,1 +1,85 @@
-console.log('TODO...');
+function start() {
+  document.getElementById("form").addEventListener("submit", createStudent);
+}
+start();
+
+async function request(url, option) {
+  const response = await fetch(url, option);
+  if (response.ok != true) {
+    const error = await response.json();
+    alert(error.message);
+    throw new Error(error.message);
+  }
+  const data = await response.json();
+  return data;
+}
+
+async function getAllStudents() {
+  const students = await request(
+    "http://localhost:3030/jsonstore/collections/students"
+  );
+
+  const rows = Object.entries(students).map(createRow).join("");
+  document.querySelector("tbody").innerHTML = rows;
+
+  function createRow([id, students]) {
+    const result = `
+        <tr data-id="${students._id}">
+            <td>${students.firstName}</td>
+            <td>${students.lastName}</td>
+            <td>${students.facultyNumber}</td>
+            <td>${students.grade}</td>
+        </tr>`;
+    return result;
+  }
+}
+
+async function createStudent(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const student = {
+    firstName: formData.get("firstName"),
+    lastName: formData.get("lastName"),
+    facultyNumber: formData.get("facultyNumber"),
+    grade: Number(formData.get("grade")),
+  };
+
+  validateData(student);
+
+  function validateData(student) {
+    if (Object.values(student).includes("")) {
+      alert("You should fill in all fields.");
+      return;
+    } else if (
+      !isNaN(Number(student.firstName) && typeof student.firstName != "string")
+    ) {
+      alert("FirstName has to be a string.");
+      return;
+    } else if (
+      !isNaN(Number(student.lastName) && typeof student.lastName != "string")
+    ) {
+      alert("LastName has to be a string.");
+      return;
+    } else if (
+      isNaN(
+        Number(student.facultyNumber) &&
+          typeof student.facultyNumber != "string"
+      )
+    ) {
+      alert("FacultyNumber has to be a number.");
+      return;
+    } else if (isNaN(student.grade)) {
+      alert("Grade has to be a score.");
+      return;
+    }
+  }
+
+  await request("http://localhost:3030/jsonstore/collections/students", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(student),
+  });
+
+  event.target.reset();
+  getAllStudents();
+}
